630449e005c4d0ac09aebf6dae14aeb0e27ec0c5
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-08-04 14:34:57 -0600

    port default

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 // Escuchar peticiones
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 });
