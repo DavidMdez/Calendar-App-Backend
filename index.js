4db46d095b09ed52c277e343927eb76d66fc69f3
DavidMdez <mdez.hdez.gamaliel@gmail.com>
2023-08-04 17:19:55 -0600

    route events

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/events', require('./routes/events'));
+app.use('/api/events', require('./routes/events'));
 
 // Escuchar peticiones
 app.listen(process.env.PORT || 3000, () => {
